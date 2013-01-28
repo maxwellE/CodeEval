cf70b53effaddc9aa41109f138d90a8a5f9a1f69
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-01-27 16:06:35 -0800

    started to write possibilities code

--- SudokuSolver/sudoku_solver.rb
+++ SudokuSolver/sudoku_solver.rb
@@ -6,17 +6,36 @@ class SudokuSolver
   end
   def generate_output_string
     parse_input
+    generate_possibilities
+    binding.pry
   end
+
   private
+
+  def generate_possibilities
+    @possibilities = Marshal.load(Marshal.dump(@grid))
+    @possibilities.each_with_index do |line,lindex|
+      line.each_with_index do |cell,cindex|
+        if cell.nil?
+          values = check_box(lindex,cindex)
+          values = values | check_column(cindex)
+          values = values | check_line(lindex)
+          @possibilities[lindex][cindex] = values
+        else
+          @possibilities[lindex][cindex] = [cell]
+        end
+      end
+    end
+  end
+
   def parse_input
     @content.split("\n").each do |e|
       next if e[0] == '-'
-      @grid.push parse_line(e)
+      @grid.push parse_line(e).flatten
     end
-    binding.pry
   end
   def parse_line(line)
-    split_line = line.split('|').collect do |x| 
+    line.split('|').collect do |x| 
       x.split('').map do |element|
         if element == "."
           nil
--- SudokuSolver/sudoku_solver_test.rb
+++ SudokuSolver/sudoku_solver_test.rb
@@ -9,6 +9,7 @@ describe SudokuSolver do
   before do 
     @ss = SudokuSolver.new("input.txt")
   end
+
   it "should find the valid solution" do
     @ss.generate_output_string.must_equal File.read("output.txt")
   end
