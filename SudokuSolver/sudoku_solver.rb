173d1575d2d776203568b9961e68813ef260b659
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-01-29 11:11:17 -0800

    generating possibilities correctly

--- SudokuSolver/sudoku_solver.rb
+++ SudokuSolver/sudoku_solver.rb
@@ -7,7 +7,6 @@ class SudokuSolver
   def generate_output_string
     parse_input
     generate_possibilities
-    binding.pry
   end
 
   private
@@ -18,14 +17,15 @@ class SudokuSolver
       line.each_with_index do |cell,cindex|
         if cell.nil?
           values = check_box(lindex,cindex)
-          values = values | check_column(cindex)
-          values = values | check_line(lindex)
+          values = check_column(cindex,values)
+          values = check_line(lindex,values)
           @possibilities[lindex][cindex] = values
         else
           @possibilities[lindex][cindex] = [cell]
         end
       end
     end
+    binding.pry
   end
 
   def parse_input
@@ -44,5 +44,46 @@ class SudokuSolver
         end
       end
     end
+  end
+  
+  def check_box(lindex,cindex)
+    line_offset = lindex.divmod(3).first
+    case line_offset
+      when 0
+        coors = ((0..2).to_a.product((0..2).to_a)) - [[lindex,cindex]]
+        extract_box_values(coors)
+      when 1
+        coors = ((3..5).to_a.product((3..5).to_a)) - [[lindex,cindex]]
+        extract_box_values(coors)
+      when 2
+        coors = ((6..-1).to_a.product((6..-1).to_a)) - [[lindex,cindex]]
+        extract_box_values(coors)
+    end
+  end
+  
+  def extract_box_values(coordinate_list)
+    possible_values = (1..9).to_a
+    coordinate_list.each do |coordinate|
+      if @grid[coordinate.first][coordinate.last]
+        possible_values = possible_values - [@grid[coordinate.first][coordinate.last]]
+      end
+    end
+    possible_values
+  end
+
+  def check_column(cindex,curent_values)
+    column_values = []
+    (0..8).each do |line_index|
+      column_values.push @grid.at(line_index).at(cindex) unless @grid.at(line_index).at(cindex).nil?
+    end
+    curent_values - column_values
+  end
+  
+  def check_line(lindex,curent_values)
+    line_values = []
+    (0..8).each do |column_index|
+      line_values.push @grid.at(lindex).at(column_index) unless @grid.at(lindex).at(column_index).nil?
+    end
+    curent_values - line_values
   end
 end
