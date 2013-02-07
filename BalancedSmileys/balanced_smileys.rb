004fe7028fc2fa1dbf3f7632f02b6e22b60c2db5
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-02-07 16:40:20 -0500

    balanced smileys passing tests

--- BalancedSmileys/balanced_smileys.rb
+++ BalancedSmileys/balanced_smileys.rb
@@ -4,7 +4,31 @@ class BalancedSmileys
     @smile_string = smile_string
   end
   
+  def is_balanced?(str,paren_count)
+    return false if paren_count < 0
+    if str == "" || str.nil?
+      return paren_count == 0
+    end
+    first_two = str[0..1]
+    if first_two == ":)"
+      return is_balanced?(str[2..-1],paren_count) || is_balanced?(str[2..-1],paren_count-1)
+    elsif first_two == ":("
+      return is_balanced?(str[2..-1],paren_count) || is_balanced?(str[2..-1],paren_count+1)
+    else
+      case str[0]
+        when "("
+          return is_balanced?(str[1..-1],paren_count+1)
+        when ")"
+          return is_balanced?(str[1..-1],paren_count-1)
+        else
+          return is_balanced?(str[1..-1],paren_count)
+      end  
+    end
+    return false
+  end
+  
   def valid?
-    binding.pry
+    return "YES" if is_balanced?(@smile_string,0)
+    "NO"
   end
 end
